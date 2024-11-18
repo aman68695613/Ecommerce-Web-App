e8db1066b402e2adc4f39a2be3d390c6ccb40ed2
aman68695613 <amansrivastav6869@gmail.com>
2024-11-18 23:35:00 +0530

    starting of a new era

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+
+
+import './App.css'
+
+function App() {
+ 
+
+  return (
+    <>
+    <div className=''>
+     <h1 className='font-bold bg-cyan-500 h-12 w-full'>Hello</h1>
+    </div>
+    </>
+  )
+}
+
+export default App
