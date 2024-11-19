16012ea256fe04c9f590a3fb8baede3640bc92be
aman68695613 <amansrivastav6869@gmail.com>
2024-11-19 20:29:56 +0530

    created services section 🗿

--- src/App.jsx
+++ src/App.jsx
@@ -4,16 +4,18 @@ import './App.css'
 import Category from './components/Category/Category';
 import Category2 from './components/Category/Category2';
 import Navbar from "./components/Navbar/Navbar";
+import Services from './components/Services/Services';
 
 function App() {
  
 
   return (
     <>
-    <div className=''>
+    <div className='bg-white dark:bg-gray-900 dark:text-white duration-200 overflow-hidden'>
      <Navbar />
      <Category/>
      <Category2/>
+     <Services/>
     </div>
     </>
   )
