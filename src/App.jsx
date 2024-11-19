a5b32f767bb7f106bb8f240b009fb44721251ad1
aman68695613 <amansrivastav6869@gmail.com>
2024-11-20 00:56:36 +0530

    created blog card section 😊👌👆

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,9 @@ import Category2 from './components/Category/Category2';
 import Services from './components/Services/Services';
 import Banner from './components/Banner/Banner';
 import headphone from "./assets/hero/headphone.png"
+import smartwatch2 from "./assets/category/smartwatch2-removebg-preview.png"
 import Products from './components/Products/Products';
+import Blogs from './components/Blogs/Blogs.jsx';
 const BannerData={
     discount: "30% OFF",
     title: "Fine Smile",
@@ -21,6 +23,16 @@ const BannerData={
     title3: "Winter Sale",
     title4: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eaque reiciendis",
     bgColor: "#f42c37"
+    }
+const BannerData2={
+    discount: "30% OFF",
+    title: "Happy Hours",
+    date: "14 Jan to 28 Jan",
+    image: smartwatch2,
+    title2: "Smart Solo ",
+    title3: "Winter Sale",
+    title4: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eaque reiciendis",
+    bgColor: "#2dcc6f"
     }
 
 function App() {
@@ -33,6 +45,8 @@ function App() {
      <Services/>
      <Banner data={BannerData}/>
      <Products/>
+     <Banner data={BannerData2}/>
+     <Blogs/>
      <Partners/>
      <Footer />
     </div>
